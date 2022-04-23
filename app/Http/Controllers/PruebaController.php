f2743adb10fd2cd58480a6b04b1e3140f94716bb
gustavo3042 <mecanicarioschillan@gmail.com>
2022-04-23 17:32:56 -0400

    PruebaSerrucho

---
+++ app/Http/Controllers/PruebaController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PruebaController extends Controller
+{
+
+public function wave_sort(){
+
+
+    $datos = array('1','2','6','19','12','3','1');
+
+
+
+echo $datos[0]."<br>";
+echo "&nbsp";
+echo $datos[6]."<br>";
+echo "&nbsp&nbsp";
+echo $datos[1]."<br>";
+echo "&nbsp&nbsp&nbsp";
+echo $datos[5]."<br>";
+echo "&nbsp&nbsp&nbsp&nbsp";
+echo $datos[2]."<br>";
+echo "&nbsp&nbsp&nbsp&nbsp&nbsp";
+echo $datos[4]."<br>";
+echo "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
+echo $datos[3]."<br>";
+
+
+
+
+
+
+}
+
+
+
+
+
+}
